0f1f2f9ae4735aa6dd77a7aeed917e2e1b25ebbf
planit <planit@tanjeffreyz.github.io>
2024-03-12 23:37:05 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -120,6 +120,13 @@ assignments = {
       "dueDate": "2024-03-19T23:59:00",
       "link": "https://www.gradescope.com/courses/690517",
       "submitted": false
+    },
+    {
+      "title": "Project Implementation and Testing",
+      "course": "CSE202_WI24_A00",
+      "dueDate": "2024-03-15T23:59:00",
+      "link": "https://www.gradescope.com/courses/690517",
+      "submitted": false
     }
   ],
   "CSE210_WI24_B00": [
