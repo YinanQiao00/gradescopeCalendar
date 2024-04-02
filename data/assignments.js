00c6d0f6259fb89f04ac2c8b81ca00f557f3cec8
planit <planit@tanjeffreyz.github.io>
2024-04-02 16:38:54 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,3 +1,11 @@
 assignments = {
-  "CSE224_SP24_A00": []
+  "CSE224_SP24_A00": [
+    {
+      "title": "Project 0: Local Sort",
+      "course": "CSE224_SP24_A00",
+      "dueDate": "2024-04-09T23:59:00",
+      "link": "https://www.gradescope.com/courses/744919",
+      "submitted": false
+    }
+  ]
 };
