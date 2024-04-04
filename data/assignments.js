aac57a1ad3dc05c285f3eeb63f49e5159a32bab7
planit <planit@tanjeffreyz.github.io>
2024-04-04 23:37:08 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,20 +1,4 @@
 assignments = {
-  "CSE 218": [
-    {
-      "title": "Week 1: Information Hiding - Readings",
-      "course": "CSE 218",
-      "dueDate": "2024-04-04T15:30:00",
-      "link": "https://www.gradescope.com/courses/762624",
-      "submitted": false
-    },
-    {
-      "title": "Week 1: Information Hiding - Discussion",
-      "course": "CSE 218",
-      "dueDate": "2024-04-04T20:30:00",
-      "link": "https://www.gradescope.com/courses/762624",
-      "submitted": false
-    }
-  ],
   "CSE224_SP24_A00": [
     {
       "title": "Project 0: Local Sort",
