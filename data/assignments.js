d816718698bbd0b9653c4ac83c3e757bc538bdaa
planit <planit@tanjeffreyz.github.io>
2024-04-05 22:37:02 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -7,5 +7,14 @@ assignments = {
       "link": "https://www.gradescope.com/courses/744919",
       "submitted": false
     }
+  ],
+  "MATH294_SP24_A00": [
+    {
+      "title": "Homework 1",
+      "course": "MATH294_SP24_A00",
+      "dueDate": "2024-04-17T23:59:00",
+      "link": "https://www.gradescope.com/courses/754653",
+      "submitted": false
+    }
   ]
 };
