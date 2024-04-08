292f815e0631ac64a0ba11097e2b6f882ff77c9e
planit <planit@tanjeffreyz.github.io>
2024-04-08 01:34:18 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -4,8 +4,8 @@ assignments = {
       "title": "Project 0: Local Sort",
       "course": "CSE224_SP24_A00",
       "dueDate": "2024-04-09T23:59:00",
-      "link": "https://www.gradescope.com/courses/744919",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/744919/assignments/4166491/submissions/244788683",
+      "submitted": true
     }
   ],
   "ECE 285": [],
