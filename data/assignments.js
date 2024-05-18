c5178e85ef834951a66eaa91c9976162604e995c
planit <planit@tanjeffreyz.github.io>
2024-05-18 07:36:20 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -27,6 +27,13 @@ assignments = {
       "dueDate": "2024-05-17T23:59:00",
       "link": "https://www.gradescope.com/courses/744919",
       "submitted": false
+    },
+    {
+      "title": "Project 4: Scalable SurfStore",
+      "course": "CSE224_SP24_A00",
+      "dueDate": "2024-05-24T23:59:00",
+      "link": "https://www.gradescope.com/courses/744919",
+      "submitted": false
     }
   ],
   "CSE256_SP24_A00": [
