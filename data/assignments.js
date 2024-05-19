221e075bb44c71a2640835222d656c0216a96a16
planit <planit@tanjeffreyz.github.io>
2024-05-19 07:36:03 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -34,6 +34,13 @@ assignments = {
       "dueDate": "2024-05-24T23:59:00",
       "link": "https://www.gradescope.com/courses/744919",
       "submitted": false
+    },
+    {
+      "title": "Project 3 Playground (Not graded)",
+      "course": "CSE224_SP24_A00",
+      "dueDate": "2024-05-24T23:59:00",
+      "link": "https://www.gradescope.com/courses/744919",
+      "submitted": false
     }
   ],
   "CSE256_SP24_A00": [
